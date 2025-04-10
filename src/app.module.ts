2b9167b72a0ea5c5c2eaeb6f9c5606b3897a6713
newton-mitro <newtonmitro@gmail.com>
2025-04-10 18:22:07 +0600

    flow doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SeederService } from './seed/seeder.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { SeedModule } from './seed/seed.module';
 import { PrismaService } from './prisma/prisma.service';
-import { ServiceModule } from './service/service.module';
-import { BeautyServiceModule } from './beauty-service/beauty-service.module';
-
+import { AuthModule } from './auth/auth.module';
+import { InvoiceModule } from './modules/invoice/invoice.module';
+import { VatModule } from './modules/vat/vat.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { LoyaltyModule } from './modules/loyalty/loyalty.module';
+import { DiscountModule } from './modules/discount/discount.module';
+import { ProductCategoryModule } from './modules/product-category/product-category.module';
+import { ServiceCategoryModule } from './modules/service-category/service-category.module';
+import { AppointmentModule } from './modules/appointment/appointment.module';
+import { UserModule } from './modules/user/user.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { SaleModule } from './modules/sale/sale.module';
+import { ProductModule } from './modules/product/product.module';
+import { BeautyServiceModule } from './modules/beauty-service/beauty-service.module';
 @Module({
-  imports: [PrismaModule, SeedModule, ServiceModule, BeautyServiceModule],
+  imports: [
+    PrismaModule,
+    SeedModule,
+    BeautyServiceModule,
+    ProductModule,
+    SaleModule,
+    PaymentModule,
+    UserModule,
+    AppointmentModule,
+    ServiceCategoryModule,
+    ProductCategoryModule,
+    DiscountModule,
+    LoyaltyModule,
+    AuthModule,
+    NotificationModule,
+    VatModule,
+    InvoiceModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService, SeederService],
+  providers: [PrismaService, SeederService],
 })
 export class AppModule {}
