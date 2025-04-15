714f0942551ff908d80fba61a4a0509cfebcb864
Newton-Mitro <newtonmitro@gmail.com>
2025-04-15 21:56:27 +0600

    seed fix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './presentation/controllers/auth.controller';
+import { JwtConfig } from '../../config/types/config.type';
 
 @Module({
+  imports: [
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const jwtConfig = configService.get<JwtConfig>('jwt')!;
+        return {
+          secret: jwtConfig.secret,
+          signOptions: {
+            expiresIn: jwtConfig.exp,
+            audience: jwtConfig.audience,
+            issuer: jwtConfig.issuer,
+            subject: 'access-token',
+            algorithm: 'HS256',
+          },
+        };
+      },
+    }),
+  ],
+  providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
